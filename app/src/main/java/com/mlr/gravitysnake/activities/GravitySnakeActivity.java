74e354d13509dd0e98da9e1e7ee03b0b2bfeead8
mlr46 <mlr46@users.noreply.github.com>
2018-12-02 23:27:04 -0500

    Fixing logic for game over

--- app/src/main/java/com/mlr/gravitysnake/activities/GravitySnakeActivity.java
+++ app/src/main/java/com/mlr/gravitysnake/activities/GravitySnakeActivity.java
@@ -271,8 +271,8 @@ public class GravitySnakeActivity extends AppCompatActivity implements SensorEve
    * @return
    */
   private boolean isGameOver(Point point) {
-    return (point.getX() >= 0 && point.getX() < screen.length
-          && point.getY() >= 0 && point.getY() < screen[0].length) ||
+    return (point.getX() < 0 || point.getX() >= screen.length
+          && point.getY() < 0 || point.getY() >= screen[0].length) ||
       screen[point.getX()][point.getY()] == Cell.SNAKE;
   }
 
